127bc1cd0b3e20e2116a47149039547d7b293183
Francary <francarymolinapaez@gmail.com>
2023-11-19 21:50:21 -0300

    05-mision Part 3

--- src/Controllers/shop.controller.js
+++ src/Controllers/shop.controller.js
@@ -1,5 +1,20 @@
+const fs = require('fs/promises'); // Módulo de manejo de archivos para Node.js
+
+
 const shopController = {
-    shop:       ( req, res ) => res.render('./shop/shop'),
+    shop:    async   ( req, res ) =>{
+       
+          // Leer el contenido del archivo JSON
+          const jsonContent = await fs.readFile("db.json", 'utf-8');
+    
+          // Parsear el contenido JSON
+          const data = JSON.parse(jsonContent);
+    
+          // Renderizar la vista con los datos (puedes pasar 'data' como variable a la vista)
+          res.render('./shop/shop', { data });
+
+    } ,
+        
     itemGet:    ( req, res ) => res.render('./shop/item'),
     itemPost:   ( req, res ) => res.send(" Route for Shop View for ID for ADD "),
     cartGet:    ( req, res ) => res.render('./shop/cart'),
