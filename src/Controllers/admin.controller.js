6a25ae78c5652bd37a67b7d3feb22d7bea5c8af1
Francary <francarymolinapaez@gmail.com>
2023-12-15 09:32:04 -0300

    06-mision part 1.2

--- src/Controllers/admin.controller.js
+++ src/Controllers/admin.controller.js
@@ -1,9 +1,24 @@
+import { getAll, getOne } from "../models/product.model.js";  
+
 const adminController ={
-    admin:          ( req, res ) => res.render("./admin/admin") ,
+    admin: async  ( req, res ) => { 
+        const data = await getAll()
+
+        res.render("./admin/admin" , {data} ) },
+
     createGet:      ( req, res ) => res.render("./admin/create") ,
+
     createPost:     ( req, res ) => res.send(" Route for Admin Create POST View") ,
-    editGet:        ( req, res ) => res.render("./admin/edit"),
+
+    editGet: async        ( req, res ) => {
+       
+        const itemId = req.params.id
+        const item =  await getOne(itemId)
+        res.render("./admin/edit", { item } )
+    },
+
     editPut:        ( req, res ) => res.send(" Route for Admin Edit ID PUT View"),
+    
     delete:         ( req, res ) => res.send(" Route for Admin View"),
 };
 
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,7 +1,8 @@
 import { sequelize } from "../config/config.js";
 
 const getAll = async () => {
-    const [dbdata] = await sequelize.query('SELECT * FROM product;')
+    // const [dbdata] = await sequelize.query('SELECT * FROM product;')
+    const [dbdata] = await sequelize.query('SELECT product.* , category.category_name, licence.licence_name FROM (product LEFT JOIN category ON product.category_id = category.category_id) LEFT JOIN licence ON product.licence_id = licence.licence_id;')
     return dbdata
 
 }
@@ -10,7 +11,8 @@ const getAll = async () => {
 
 
 const getOne = async (id) => {
-    const [dbdata] = await sequelize.query('SELECT * FROM product WHERE product_id=?', {
+    // const [dbdata] = await sequelize.query('SELECT * FROM product WHERE product_id=?', {
+    const [dbdata] = await sequelize.query('SELECT product.* , category.category_name, licence.licence_name FROM (product LEFT JOIN category ON product.category_id = category.category_id) LEFT JOIN licence ON product.licence_id = licence.licence_id WHERE product_id=? ;', {
         replacements:[id], 
         type: sequelize.QueryTypes.SELECT
     });
