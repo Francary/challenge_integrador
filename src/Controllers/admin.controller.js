2fec18b7402532c3401fc3a9380a0bccb0447c2b
Francary <francarymolinapaez@gmail.com>
2023-12-17 18:46:35 -0300

    mision-06 Edit Producto Mejorado

--- src/Controllers/admin.controller.js
+++ src/Controllers/admin.controller.js
@@ -55,7 +55,7 @@ const adminController ={
         const verificarImagenes = req.files.length !== 0;
         const itemId = req.params.id
         const item =  await getOne(itemId)
-        const product_shema = verificarImagenes ? {
+        const product_shema =  {
             
             product_name: req.body.product_name.toUpperCase(),
             product_description: req.body.description,
@@ -64,29 +64,14 @@ const adminController ={
             discount: Number(req.body.discount),
             sku: req.body.sku,
             dues: Number(req.body.dues),
-            image_front: `/img/products/${req.files[0].filename}` ,
-            image_back: `/img/products/${req.files[1].filename}`,
+            image_front:  verificarImagenes ? `/img/products/${req.files[0].filename}` : item.image_front ,
+            image_back: verificarImagenes ? `/img/products/${req.files[1].filename}` : item.image_back,
             licence_id: Number(req.body.licence),
             category_id: Number(req.body.category),
             
             
         }
-        :{
-            
-            product_name: req.body.product_name.toUpperCase(),
-            product_description: req.body.description,
-            price: Number(req.body.price),
-            stock: Number(req.body.stock),
-            discount: Number(req.body.discount),
-            sku: req.body.sku,
-            dues: Number(req.body.dues),
-            image_front: item.image_front ,
-            image_back: item.image_back,
-            licence_id: Number(req.body.licence),
-            category_id: Number(req.body.category),
-            
-            
-        }
+       
         const result =  await editItem(product_shema, itemId)
             
         res.redirect('/admin/admin')
